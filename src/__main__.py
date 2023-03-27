24f43e52ed7448c06bfc5a1ab887562f9ae36043
Artemiy Lazarev <artemiy.lazarevx7@gmail.com>
2023-03-27 07:05:32 +0300

    Task 3

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,9 @@
 import random
-import task1, task2
+import task1, task2, task3
 
 if __name__ == '__main__':
     random.seed()
     task1.task1()
     task2.task2()
+    task3.task3()
 
---
+++ src/task3.py
@@ -0,0 +1,37 @@
+import random
+import numpy as np
+
+
+def studied(num: int):
+    return 0 <= num < 20
+
+
+def play(second: bool = False) -> bool:
+    tickets = list(range(30))
+
+    if second:
+        ch = random.choice(tickets)
+        tickets.remove(ch)
+
+    ch = random.choice(tickets)
+    return ch < 20
+
+
+def task3():
+    sample_size: int = 1_000_000
+
+    success_first: int = 0
+    success_second: int = 0
+
+    for _ in range(sample_size):
+        if play():
+            success_first += 1
+        if play(second=True):
+            success_second += 1
+
+    print(f'First, chance: {success_first / sample_size}')
+    print(f'First, chance: {success_second / sample_size}')
+
+
+if __name__ == '__main__':
+    task3()
